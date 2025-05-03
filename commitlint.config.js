2cdbf716b2910923b605b775ed306b08a779e745
mytoandeptrai <mytoandn@gmail.com>
2025-05-04 01:45:53 +0700

    feat: set up husky and commit lint

---
+++ commitlint.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',     // New feature
+        'fix',      // Bug fix
+        'docs',     // Documentation changes
+        'style',    // Code style changes (formatting, etc)
+        'refactor', // Code refactoring
+        'test',     // Adding or modifying tests
+        'chore',    // Changes to build process or auxiliary tools
+        'perf',     // Performance improvements
+        'ci',       // CI configuration changes
+        'revert'    // Revert a previous commit
+      ]
+    ]
+  }
+} 
